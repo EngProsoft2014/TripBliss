ff9cb1f3ca9f23ca6b0bd5b6291dfaaafc42fbf4
MR.Akl <MR.Akl@DESKTOP-01O5FDQ>
2024-09-07 01:11:48 +0300

    07/09/2024|ِAbdullah

--- Models/ResponseWithDistributorPayment/ResponseWithDistributorPaymentResponse.cs
+++ Models/ResponseWithDistributorPayment/ResponseWithDistributorPaymentResponse.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public int ResponseWithDistributorId { get; set; }
+        public string? RequestName { get; set; }
+        public string? DistributorCompanyName { get; set; }
+        public string? TravelAgencyCompanyName { get; set; }
         public int? PaymentMethod { get; set; }
         public int? dbcr { get; set; }
         public int? AmountPayment { get; set; }
@@ -11,3 +14,4 @@
         public string? Notes { get; set; }
     }
 }
+
