b31821cf414fd768288432ffbebfd5f3984c1370
AhmedFayez299 <93268385+AhmedFayez299@users.noreply.github.com>
2024-12-18 17:22:47 +0200

    update

---
+++ Platforms/iOS/CustomContentPageRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+
+using Foundation;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Platform.iOS;
+using UIKit;
+using TripBliss.Platforms.iOS;
+
+[assembly: Microsoft.Maui.Controls.Compatibility.ExportRenderer(typeof(ContentPage), typeof(CustomContentPageRenderer))]
+namespace TripBliss.Platforms.iOS
+{
+    public class CustomContentPageRenderer : PageRenderer
+    {
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            var tapGestureRecognizer = new UITapGestureRecognizer(OnTapped)
+            {
+                CancelsTouchesInView = false
+            };
+            View.AddGestureRecognizer(tapGestureRecognizer);
+        }
+
+        private void OnTapped()
+        {
+            View.EndEditing(true);
+        }
+    }
+}
