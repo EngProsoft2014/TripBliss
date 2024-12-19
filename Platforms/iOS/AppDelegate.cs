f587ace1d98d0abf95a727916ca65733a5d088ec
AhmedFayez299 <93268385+AhmedFayez299@users.noreply.github.com>
2024-12-19 23:28:00 +0200

    update

--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using UIKit;
 
 namespace TripBliss
 {
@@ -6,5 +7,28 @@ namespace TripBliss
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            var result = base.FinishedLaunching(application, launchOptions);
+
+            // Get the main window
+            var window = UIApplication.SharedApplication.KeyWindow;
+
+            if (window != null)
+            {
+                // Add a tap gesture recognizer to dismiss the keyboard
+                var tapRecognizer = new UITapGestureRecognizer(() =>
+                {
+                    window.EndEditing(true); // Dismiss the keyboard
+                })
+                {
+                    CancelsTouchesInView = false // Ensure other UI interactions are not blocked
+                };
+                window.AddGestureRecognizer(tapRecognizer);
+            }
+
+            return result;
+        }
     }
 }
